5d0246e85aaffe2b99a1f63451e9c5e1dbdd6932
letzho <122744653+letzho@users.noreply.github.com>
2025-01-01 13:29:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,9 +36,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",  # Add this line
-        "http://127.0.0.1:5173",
+        "http://localhost:3000",
         "http://127.0.0.1:5174",  # Add this line too for completeness
     ],
     allow_credentials=True,
